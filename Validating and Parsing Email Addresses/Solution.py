1c980e137735e020b07cf1644890b04226806b74
Athulya P D <47109341+JNightshadow@users.noreply.github.com>
2025-07-04 07:18:22 +0000

    Solved

---
+++ Validating and Parsing Email Addresses/Solution.py
@@ -0,0 +1,9 @@
+import email.utils
+import re
+n = int(input())
+invalid = False
+for i in range(n):
+    addr = input()
+    addrCheck = email.utils.parseaddr(addr) 
+    if(re.match(r'^[a-zA-Z]+[a-zA-Z-\._0-9]*@[a-zA-Z]+\.[a-zA-Z]{1,3}$',addrCheck[1])):
+        print(addr)
